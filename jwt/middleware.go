1e0aacc9f873e06e26f2108c81f251eadfdab600
Brian Ketelsen <bketelsen@gmail.com>
2015-12-08 09:44:23 -0500

    WIP

--- jwt/middleware.go
+++ jwt/middleware.go
@@ -1,21 +1,92 @@
 package jwt
 
 import (
+	"net/http"
+	"strings"
 	"time"
 
 	jwt "github.com/dgrijalva/jwt-go"
 	"github.com/raphael/goa"
 )
 
-const ()
+// JWTKey is the JWT middleware key used to store the token in the context.
+const JWTKey middlewareKey = 0
 
-func JWTMiddleware(ttl int, secret string) Middleware {
-	return func(h goa.Handler) Handler {
-		return func(ctx *Context) (err error) {
+// middlewareKey is the private type used for goa middlewares to store values in the context.
+// It is private to avoid possible collisions with keys used by other packages.
+type middlewareKey int
+
+// JWTHeader is the name of the header used to transmit the JWT token.
+const JWTHeader = "Authorization"
+
+type Specification struct {
+	// TokenHeader is the HTTP header to search for the JWT Token
+	// Defaults to "Authorization"
+	TokenHeader string
+	// TokenParam is the request parameter to parse for the JWT Token
+	// Defaults to "token"
+	TokenParam string
+	// AllowParam is a flag that determines whether it is allowable
+	// to parse tokens from the querystring
+	// Defaults to false
+	AllowParam bool
+	// ValidationFunc is a function that returns the key to validate the JWT
+	// See github.com/dgrijalva/jwt for specification
+	// Required, no default
+	ValidationFunc jwt.Keyfunc
+	// AuthOptions is a flag that determines whether a token is required on OPTIONS
+	// requests
+	AuthOptions bool
+}
+
+// JWTMiddleware is a middleware that injects retrieves a JWT token from the request if present and
+// injects it into the context.  It checks for the token in the HTTP Headers first, then the querystring if
+// the specification "AllowParam" is true.
+// Retrieve it using ctx.Value(JWTKey).
+func JWTMiddleware(spec Specification) goa.Middleware {
+	return func(h goa.Handler) goa.Handler {
+		return func(ctx *goa.Context) error {
+
+			// If AuthOptions is false, and this is an OPTIONS request
+			// just let the request fly
+			if !spec.AuthOptions && ctx.Request().Method == "OPTIONS" {
+				return h(ctx)
+			}
+
+			if spec.TokenHeader == "" {
+				spec.TokenHeader = "Authorization"
+			}
+			if spec.TokenParam == "" {
+				spec.TokenParam = "token"
+			}
+			var found bool
+			var token string
+			header := ctx.Request().Header.Get(spec.TokenHeader)
+
+			if header != "" {
+				parts := strings.Split(header, " ")
+				if len(parts) != 2 || strings.ToLower(parts[0]) != "bearer" {
+					// This is an error
+				}
+				token = parts[1]
+				found = true
+			}
+			if !found && spec.AllowParam {
+				token = ctx.Request().URL.Query().Get(spec.TokenParam)
+			}
+
+			if token == "" {
+				err := ctx.Respond(http.StatusUnauthorized, []byte(http.StatusText(http.StatusUnauthorized)))
+				return err
+			}
+
+			ctx.SetValue(JWTKey, token)
+
 			return h(ctx)
 		}
 	}
 }
+
 func Token(claims map[string]string) (string, error) {
 	// create a signer for rsa 256
 	t := jwt.New(jwt.GetSigningMethod("RS256"))
--- jwt/middleware_test.go
+++ jwt/middleware_test.go
@@ -1,155 +1,90 @@
 package jwt_test
 
 import (
-	"fmt"
 	"net/http"
-	"time"
+	"strings"
 
-	"github.com/julienschmidt/httprouter"
+	"gopkg.in/inconshreveable/log15.v2"
+
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 	"github.com/raphael/goa"
-	"github.com/raphael/goa/cors"
-	"gopkg.in/inconshreveable/log15.v2"
+	"github.com/raphael/goa/jwt"
 )
 
-var _ = Describe("Middleware", func() {
+var _ = Describe("JWT Middleware", func() {
+	var handler *testHandler
+	var ctx *goa.Context
+	var spec jwt.Specification
+	var req *http.Request
+	var err error
+	params := map[string]string{"param": "value"}
+	query := map[string][]string{"query": []string{"qvalue"}}
+	payload := map[string]interface{}{"payload": 42}
 
-	Context("with a running goa app", func() {
-		var dsl func()
-		var method string
-		var path string
-		var optionsHandler goa.Handler
+	BeforeEach(func() {
+		req, err = http.NewRequest("POST", "/goo", strings.NewReader(`{"payload":42}`))
+		Ω(err).ShouldNot(HaveOccurred())
+		rw := new(TestResponseWriter)
+		ctx = goa.NewContext(nil, req, rw, params, query, payload)
+		handler = new(testHandler)
+		logger := log15.New("test", "test")
+		logger.SetHandler(handler)
+		ctx.Logger = logger
+		spec = jwt.Specification{
+			AllowParam: true,
+		}
+	})
 
-		var service *goa.GracefulApplication
-		var url string
-		portIndex := 1
+	It("requires a jwt token be present", func() {
+		h := func(ctx *goa.Context) error {
+			ctx.JSON(200, "ok")
+			return nil
+		}
+		jw := jwt.JWTMiddleware(spec)(h)
+		Ω(jw(ctx)).ShouldNot(HaveOccurred())
+		Ω(ctx.ResponseStatus()).Should(Equal(http.StatusUnauthorized))
 
-		JustBeforeEach(func() {
-			goa.Log.SetHandler(log15.DiscardHandler())
-			service = goa.NewGraceful("").(*goa.GracefulApplication)
-			spec, err := cors.New(dsl)
-			Ω(err).ShouldNot(HaveOccurred())
-			service.Use(cors.Middleware(spec))
-			router := service.HTTPHandler().(*httprouter.Router)
-			h := func(ctx *goa.Context) error { return ctx.Respond(200, nil) }
-			ctrl := service.NewController("test")
-			router.Handle(method, path, ctrl.NewHTTPRouterHandle("", h))
-			router.Handle("OPTIONS", path, ctrl.NewHTTPRouterHandle("", optionsHandler))
-			cors.MountPreflightController(service, spec)
-			portIndex++
-			port := 54511 + portIndex
-			url = fmt.Sprintf("http://localhost:%d", port)
-			go service.ListenAndServe(fmt.Sprintf(":%d", port))
-			// ugh - does anyone have a better idea? we need to wait for the server
-			// to start listening or risk tests failing because sendind requests too
-			// early.
-			time.Sleep(time.Duration(100) * time.Millisecond)
-		})
+	})
 
-		AfterEach(func() {
-			service.Shutdown()
-		})
-
-		Context("handling GET requests", func() {
-			BeforeEach(func() {
-				method = "GET"
-				path = "/"
-			})
-
-			It("responds", func() {
-				resp, err := http.Get(url)
-				Ω(err).ShouldNot(HaveOccurred())
-				Ω(resp.StatusCode).Should(Equal(200))
-			})
-
-			Context("using CORS that allows the request", func() {
-				BeforeEach(func() {
-					dsl = func() {
-						cors.Origin("http://authorized.com", func() {
-							cors.Resource("/", func() {
-								cors.Methods("GET")
-							})
-						})
-					}
-				})
-
-				It("sets the Acess-Control-Allow-Methods header", func() {
-					req, err := http.NewRequest("GET", url, nil)
-					Ω(err).ShouldNot(HaveOccurred())
-					req.Header.Set("Origin", "http://authorized.com")
-					resp, err := http.DefaultClient.Do(req)
-					Ω(err).ShouldNot(HaveOccurred())
-					Ω(resp.StatusCode).Should(Equal(200))
-					Ω(resp.Header).Should(HaveKey("Access-Control-Allow-Methods"))
-				})
-
-			})
-
-			Context("using CORS that disallows the request", func() {
-				BeforeEach(func() {
-					dsl = func() {
-						cors.Origin("http://authorized.com", func() {
-							cors.Resource("/", func() {
-								cors.Methods("POST")
-							})
-						})
-					}
-				})
-
-				It("does not set the Acess-Control-Allow-Methods header", func() {
-					req, err := http.NewRequest("GET", url, nil)
-					Ω(err).ShouldNot(HaveOccurred())
-					req.Header.Set("Origin", "http://nonauthorized.com")
-					resp, err := http.DefaultClient.Do(req)
-					Ω(err).ShouldNot(HaveOccurred())
-					Ω(resp.StatusCode).Should(Equal(200))
-					Ω(resp.Header).ShouldNot(HaveKey("Access-Control-Allow-Methods"))
-				})
-
-			})
-
-			Context("using a CORS preflight request", func() {
-				BeforeEach(func() {
-					dsl = func() {
-						cors.Origin("http://authorized.com", func() {
-							cors.Resource("/", func() {
-								cors.Methods("GET")
-							})
-						})
-					}
-				})
-
-				It("sets the Acess-Control-Allow-Methods header when no OPTION action exists", func() {
-					req, err := http.NewRequest("OPTIONS", url, nil)
-					Ω(err).ShouldNot(HaveOccurred())
-					req.Header.Set("Origin", "http://authorized.com")
-					req.Header.Set("Access-Control-Request-Method", "GET")
-					resp, err := http.DefaultClient.Do(req)
-					Ω(err).ShouldNot(HaveOccurred())
-					Ω(resp.StatusCode).Should(Equal(200))
-					Ω(resp.Header).Should(HaveKey("Access-Control-Allow-Methods"))
-				})
-
-				Context("with an OPTIONS action", func() {
-					BeforeEach(func() {
-						optionsHandler = func(ctx *goa.Context) error { return ctx.Respond(200, nil) }
-					})
-
-					It("sets the Acess-Control-Allow-Methods header when OPTION actions exist", func() {
-						req, err := http.NewRequest("OPTIONS", url, nil)
-						Ω(err).ShouldNot(HaveOccurred())
-						req.Header.Set("Origin", "http://authorized.com")
-						req.Header.Set("Access-Control-Request-Method", "GET")
-						resp, err := http.DefaultClient.Do(req)
-						Ω(err).ShouldNot(HaveOccurred())
-						Ω(resp.StatusCode).Should(Equal(200))
-						Ω(resp.Header).Should(HaveKey("Access-Control-Allow-Methods"))
-					})
-				})
-
-			})
-
-		})
+	It("returns the jwt token that was sent", func() {
+		req.Header.Set("Authorization", "bearer TOKEN")
+		h := func(ctx *goa.Context) error {
+			ctx.JSON(200, "ok")
+			return nil
+		}
+		jw := jwt.JWTMiddleware(spec)(h)
+		Ω(jw(ctx)).ShouldNot(HaveOccurred())
+		Ω(ctx.ResponseStatus()).Should(Equal(http.StatusOK))
+		Ω(ctx.Value(jwt.JWTKey)).Should(Equal("TOKEN"))
+		Ω(ctx.Value(jwt.JWTKey)).ShouldNot(Equal("bearer TOKEN"))
 	})
 })
+
+type testHandler struct {
+	Records []*log15.Record
+}
+
+func (t *testHandler) Log(r *log15.Record) error {
+	t.Records = append(t.Records, r)
+	return nil
+}
+
+type TestResponseWriter struct {
+	ParentHeader http.Header
+	Body         []byte
+	Status       int
+}
+
+func (t *TestResponseWriter) Header() http.Header {
+	return t.ParentHeader
+}
+
+func (t *TestResponseWriter) Write(b []byte) (int, error) {
+	t.Body = append(t.Body, b...)
+	return len(b), nil
+}
+
+func (t *TestResponseWriter) WriteHeader(s int) {
+	t.Status = s
+}
